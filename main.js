075e19f5fc667354c4b99c0cf81663a951f63cef
Alexandru Ramon Miron <60828762+mironalex00@users.noreply.github.com>
2024-01-05 10:14:07 +0100

    Create main.js
    Añadida función en el script de JS para navegador web que facilita el envío de N mensajes.
    
    ¡Optimizandas la interacción del usuario con esta nueva funcionalidad!

---
+++ main.js
@@ -0,0 +1,262 @@
+async function enviarScript(text, lines, debugging = false){
+
+	// Define URL checker
+	const isUrl = () => {
+		try { return Boolean(new URL(text)); }
+		catch(e){ return false; }
+	}
+	
+	//	Define Send button finder
+	const findSendBtn = () => {
+	
+		//	If debugging show message
+		if(debugging) console.warn("Trying to find the button");
+		
+		//	Try to find the button
+		let sendBtn = (main.querySelector(`[aria-label="Send"]`) || main.querySelector(`[data-icon="send"]`));
+
+		//	If not exist, find send button
+		if( !sendBtn ) {
+			const divs = document.querySelectorAll('div[role="button"]');
+			divs.forEach(div => {
+				if (div.textContent.includes("Send")) sendBtn = div
+			} );
+		}
+		
+		//	If debugging show message
+		if(!(!sendBtn) && debugging) console.log("Button founded") 
+		
+		//	Return
+		return sendBtn;
+	
+	}
+	
+	//	Define Send via key dispatch event
+	const sendViaIntro = async ( message ) => {
+		//	Focus textarea loc element
+		textarea.focus();
+		// Wait to make sure is focus
+		await new Promise(resolve => setTimeout(resolve, 100));
+		document.execCommand('insertText', false, ( message ) );
+		//	Dispatch event
+		textarea.dispatchEvent(new Event('change', {bubbles: true}));
+		// Wait to make sure is typed
+		await new Promise(resolve => setTimeout(resolve, 100));
+		//	Dispatch send vent
+		textarea.dispatchEvent(new KeyboardEvent('keydown', {
+			keyCode: 13,
+			bubbles: true
+		}));
+	}
+	
+	//
+	const getTokenFromStore = function () {
+	
+		if(!window.sessionStorage) {
+		
+			// If debugging show message
+			if(debugging) console.warn("SessionStorage not defined, creating");
+			
+			// Create object
+			window.sessionStorage = {
+				
+				//	Data
+				data: {},
+				
+				//	Get Item
+				getItem(key) {
+					return this.data[key] || null;
+				},
+				
+				//	Set Item
+				setItem(key, value) {
+					this.data[key] = value;
+				},
+				//	Remove item
+				removeItem(key) {
+					delete this.data[key];
+				},
+				
+				// Clear
+				clear() {
+					this.data = {};
+				}
+				
+			}
+
+		}
+		
+		//	Variable definitions
+		let listening = true;
+
+		//	Constant definitions
+		const targetURL = "https://discord.com/api/v9/science";
+		const token = window.sessionStorage.getItem("token");
+		
+		// If token exists return the token
+		if(token) return token;
+
+		// Create function that intercepts the token
+		function interceptDiscordScience() {
+
+			// Intercept the XMLHttpRequest
+			let originalXHR = window.XMLHttpRequest;
+
+			//	Create function that generates new XHR
+			function nuevaXHR() {
+
+				// Define xhr
+				let xhr = new originalXHR();
+				let originalOpen = xhr.open;
+
+				//	Open xhr
+				xhr.open = function (tipo, url) {
+					if (tipo.toLowerCase() === 'post' && url === targetURL && listening) {
+						console.log('¡Se detectó una petición POST a la URL especificada!');
+						console.log('URL: ' + url);
+						// Aquí puedes realizar acciones adicionales si se detecta una petición POST
+					}
+					//	Apply the request
+					originalOpen.apply(xhr, arguments);
+				};
+
+				// Define the request headers
+				let originalSetRequestHeader = xhr.setRequestHeader;
+				
+				//	Set the reqquest headers
+				xhr.setRequestHeader = function (header, value) {
+					// Check header existance and save the value
+					if (header.toLowerCase() === 'authorization') {
+						window.sessionStorage.setItem("token", value);
+						console.warn("Token found (%s), saving", value)
+					}
+					//	Send
+					originalSetRequestHeader.apply(xhr, arguments);
+				};
+				//	
+				return xhr;
+			}
+
+			// Sobrescribimos la interfaz XMLHttpRequest
+			window.XMLHttpRequest = nuevaXHR;
+		}
+
+		//	If listening intercept
+		if(listening) interceptDiscordScience();
+		
+	}
+	
+	//	Define Send via fetch vent
+	const sendViaRequest = async ( message ) => {
+		const token = getTokenFromStore();
+		// Define header
+		const headers = {
+			'Authorization': token,
+			'Content-Type': "application/json",
+			'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+		}
+		//	Define body
+		const body = JSON.stringify({
+		  "mobile_network_type": "unknown",
+		  "content": message,
+		  "tts": false,
+		  "flags": 0
+		});
+		// Define request options
+		const requestOptions = {
+		  method: 'POST',
+		  headers,
+		  body,
+		};
+		//	Save current channel id
+		const channel_id = window.location.pathname.split('/').pop();
+		//  Save response 
+		const response = await fetch(`https://discord.com/api/v9/channels/${channel_id}/messages`, requestOptions);
+		// If response not OK, reject promise
+		if(!response.ok) {		
+			// Rechaza la promesa con el error capturado
+			return Promise.reject("No se pudo enviar el mensaje...");
+		}
+	}
+	
+	//	Define function to check wether user is on chat or not
+	const isUserOnChat = () => {
+		//
+		const d_regex = /^https:\/\/discord\.com\/channels\/[\w@]+\/\d{10,}$/gm;
+		const i_regex = /^https:\/\/www\.instagram\.com\/direct\/t\/\d{10,}\/?$/gm;
+		const w_regex = /^https:\/\/web\.whatsapp\.com\/$/gm;
+		//	
+		switch (true) {
+			case d_regex.test(window.location.href):
+				return 'discord';
+			case i_regex.test(window.location.href):
+				return 'instagram';
+			case w_regex.test(window.location.href):
+				return 'whatsapp';
+			default:
+				return false;
+		}
+	}
+
+	// Save HTML loc
+	const main = (document.querySelector("#main") || document.querySelector("div[role='main']"));
+	const textarea = (main?.querySelector(`div[contenteditable="true"]`) || document.querySelector('div[class*=textAreaSlate] > div > div[role="textbox"]') );
+	const userService = isUserOnChat();
+	
+	//	Define text clean var with default value (text)
+	let resTextClean = text;
+	
+	//	Error handling
+	if(!text) throw new Error("Debes indicar el texto a enviar...");
+	if( userService === false || !textarea) throw new Error("No hay conversación abierta");
+	
+	// If recieved string
+	if( typeof text === 'string' || text instanceof String) {
+		// Create pattern
+		const pattern = /[\n\t]+/;
+		// If multiline string
+		if(pattern.test(resTextClean)) {
+			resTextClean = text.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+		}else {
+			if( !lines || lines <= 0 ) throw new Error( "Debes indicar cuantas veces se manda el mensaje");
+		}
+	}else {
+		resTextClean = resTextClean.map(line => line.trim()).filter(line => line);
+	}
+	
+	//	
+	console.log("Is 'text' param URL: %b", isUrl()); // TODO: Implement file downloader (JSON AND PLAIN-TEXT)
+	
+	//	Save if text clean is array
+	const isTextArray = Array.isArray( resTextClean );
+	
+	//	Define lines
+	lines = (( isTextArray ?? lines > resTextClean.length ? null : lines ) ?? resTextClean.length);
+	
+	//	Loop through text
+	for(let i = 0; i < ( lines ); i++){
+		
+		//	Define what text gonna be
+		const tempText = isTextArray ? resTextClean[i] : resTextClean;
+		
+		//	Skip if empty
+		if(!tempText || tempText.length <= 0) continue;
+		
+		//	Define percentage
+		const currentPercentage = (i + 1) * 100 / lines;
+		
+		//	Define debug_message 
+		const debug_message = !debugging ? '' : `: ${parseInt(currentPercentage)} %`;
+		
+		//	Check percentage
+		console.log("%s %d%, Is user on chat: %s", tempText, currentPercentage, userService );
+		
+		//	Send message via intro when boolean, otherwise via request (for discord)
+		if( userService === 'discord') await sendViaRequest(  tempText + debug_message );
+		else await sendViaIntro( tempText + debug_message );
+		
+	}
+	
+	// Return the value
+	return `I have sent ${lines} lines in total`;
+}
